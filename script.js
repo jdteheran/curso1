d557eb8fc6e5ebe6649dfaec3042cd9db98ee656
Juan Teheran <juante@tirant.com>
2022-02-07 18:58:04 -0500

    algoritmo de los primos en veremos

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ for (let i = 2; i <= num_factorial; i++) {
     factorial = factorial * i
 }
 
+let primo = true
+
+for (let i = 1; i <=num_factorial; i++) {
+    if (num_factorial % i == 0) {
+        primo = false
+    }
+    
+}
+
 console.log(factorial);
+console.log(primo);
