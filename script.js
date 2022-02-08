4487944bde852bb01e1280ca998ce3c862dfb1f1
Juan Teheran <juante@tirant.com>
2022-02-07 19:20:28 -0500

    si primo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const num_factorial = 6
+const num_factorial = 71
 
 let factorial = 1
 
@@ -9,7 +9,7 @@ for (let i = 2; i <= num_factorial; i++) {
 
 let primo = true
 
-for (let i = 1; i <=num_factorial; i++) {
+for (let i = 2; i < num_factorial; i++) {
     if (num_factorial % i == 0) {
         primo = false
     }
