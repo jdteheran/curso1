c4be872ce7e8f3f1f15115c2f2015c230de8187f
Juan Teheran <juante@tirant.com>
2022-02-07 19:10:37 -0500

    no fibo

--- script.js
+++ script.js
@@ -7,4 +7,16 @@ for (let i = 2; i <= num_factorial; i++) {
     factorial = factorial * i
 }
 
+
+let a = 1
+let b = 1
+let c = 0
+
+for (let i = 0; i < num_factorial; i++) {
+    c = a + b
+    b = c
+    a = b
+}
+
 console.log(factorial);
+console.log(c);
